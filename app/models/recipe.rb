a51d05124f26293d030dcbabc0673d33b35bb565
Pierre GIRARDEAU <pierre.girardeau@hotmail.fr>
2023-11-14 23:23:26 +0100

    🌱 Add mysql config, initial models and seed data

---
+++ app/models/recipe.rb
@@ -0,0 +1,5 @@
+class Recipe < ApplicationRecord
+    has_many :recipe_ingredients, class_name: 'RecipeIngredient'
+    has_many :ingredients, through: :recipe_ingredients
+  end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'json'
+require_relative '../app/models/recipe'
+require_relative '../app/models/ingredient'
+
+recipes_data = JSON.parse(File.read(Rails.root.join('db', 'recipes-en.json')))
+
+recipes_data.each do |recipe_data|
+  # Create recipe record
+  recipe = Recipe.create!(
+    title: recipe_data['title'],
+    cook_time: recipe_data['cook_time'],
+    prep_time: recipe_data['prep_time'],
+    ratings: recipe_data['ratings'],
+    cuisine: recipe_data['cuisine'],
+    category: recipe_data['category'],
+    author: recipe_data['author'],
+    image: recipe_data['image']
+  )
+
+  # Create ingredients and associate with the recipe
+  recipe_data['ingredients'].each do |ingredient_name|
+    ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+    recipe.ingredients << ingredient
+  end
+
+  puts "Added Recipe: #{recipe.title} (ID: #{recipe.id})"
+end
